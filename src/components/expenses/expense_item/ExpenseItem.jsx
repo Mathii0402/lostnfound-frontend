72a990a2e17018a72919be833116cb6740bac45d
Nifaz <64573671+nifazzz10@users.noreply.github.com>
2023-03-02 22:33:33 +0530

    Frontend UI Final Commit

--- src/components/expenses/expense_item/ExpenseItem.jsx
+++ src/components/expenses/expense_item/ExpenseItem.jsx
@@ -1,72 +1,73 @@
-import React, { useState } from "react"
-import Axios from 'axios';
-import './ExpenseItem.css';
-import ExpenseDate from "../expense_date/ExpenseDate";
+import React, { useState } from "react";
+import Axios from "axios";
+import "./ExpenseItem.css";
+
 import Card from "../../ui/Card";
 import AlertDialog from "../../founddialogue/Founddialogue";
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography'
+import { styled } from "@mui/material/styles";
+
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
 import { useEffect } from "react";
 const ExpenseItem = (props = {}) => {
   console.log(props);
   const [color, setColor] = useState();
-  const [text, setText] = useState('Found?🔎');
-  const [styling, setstyling] = useState('none')
-  const [disable,setdisable] = useState(false);
+  const [text, setText] = useState("Found?🔎");
+  const [styling, setstyling] = useState("none");
+  const [disable, setdisable] = useState(false);
   const { date, title, amount, name, desc, place, objid } = props;
   const [find, setFind] = useState(false);
   const clickHandler = () => {
-    setFind('found');
-
-
+    setFind("found");
   };
   const apicall = () => {
-    Axios.get(`https://lostnfound-api-backend.onrender.com/api/v1/object/${objid}`)
-    .then((res) => {
+    Axios.get(
+      `https://lostnfound-api-backend.onrender.com/api/v1/object/${objid}`
+    ).then((res) => {
       const oid = res.data.data[0].objid;
       if (oid == objid) {
         setColor("green");
         setText("Founded !");
 
-        let ans = res.data.data[0]
+        let ans = res.data.data[0];
 
         setdatas(ans);
         setdisable(true);
-        setstyling("content")
+        setstyling("content");
       }
-
-    })
-  }
-  const [datas, setdatas] = useState({})
+    });
+  };
+  const [datas, setdatas] = useState({});
 
   const HtmlTooltip = styled(({ className, ...props }) => (
     <Tooltip {...props} classes={{ popper: className }} />
   ))(({ theme }) => ({
     [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#f5f5f9',
-      color: 'rgba(0, 0, 0, 0.87)',
+      background: 'rgba(59, 58, 58, 0.95)',
+     
+      borderRadius:'16px',
+      fontFamily:"monospace",
+      color: "white",
       display: `${styling}`,
       maxWidth: 220,
       fontSize: theme.typography.pxToRem(12),
-      border: '1px solid #dadde9',
+    
     },
   }));
   useEffect(() => {
-    apicall()
+    apicall();
   }, []);
   return (
-
-    <Card >
+    <Card>
       <HtmlTooltip
         title={
           <React.Fragment>
-            <Typography color="inherit">Tooltip with HTML</Typography>
-            <p>{datas.name}</p>
-            <p>{datas.number}</p>
-            <p>{datas.place}</p>
-            <p>{datas.address}</p>
+ 
+            <h3>Object Founded Person's Details</h3>
+            <p>Name:{datas.name}</p>
+            <p>Mobile Number:{datas.number}</p>
+            <p>Place:{datas.place}</p>
+            <p>Address:{datas.address}</p>
           </React.Fragment>
         }
       >
@@ -76,44 +77,64 @@ const ExpenseItem = (props = {}) => {
               <label htmlFor="">Name:</label>
             </div>
             <div>
-              <h2>{name}</h2></div>
+              <h2>{name}</h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Obj Id:</label></div>
-            <div><h2> {objid} </h2></div>
+            <div>
+              <label htmlFor="">Obj Id:</label>
+            </div>
+            <div>
+              <h2> {objid} </h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Lost object:</label></div>
-            <div><h2> {desc} </h2></div>
+            <div>
+              <label htmlFor="">Lost object:</label>
+            </div>
+            <div>
+              <h2> {desc} </h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Desc:</label></div>
-            <div><h2> {title} </h2></div>
+            <div>
+              <label htmlFor="">Desc:</label>
+            </div>
+            <div>
+              <h2> {title} </h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Lost Date:</label></div>
-            <div><h2> {date} </h2></div>
+            <div>
+              <label htmlFor="">Lost Date:</label>
+            </div>
+            <div>
+              <h2> {date} </h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Mobile Number:</label></div>
-            <div><h2> {amount} </h2></div>
+            <div>
+              <label htmlFor="">Mobile Number:</label>
+            </div>
+            <div>
+              <h2> {amount} </h2>
+            </div>
           </div>
           <div className="inline">
-            <div><label htmlFor="">Place:</label></div>
-            <div><h2> {place} </h2></div>
-          </div >
+            <div>
+              <label htmlFor="">Place:</label>
+            </div>
+            <div>
+              <h2> {place} </h2>
+            </div>
+          </div>
           <div className="inline">
             <AlertDialog color={color} disabled={disable} text={text} />
-
           </div>
-
         </div>
-
       </HtmlTooltip>
-      {/* <ExpenseDate  date={date} /> */}
-
     </Card>
-  )
-}
+  );
+};
 
 export default ExpenseItem;
--- src/components/founddialogue/Founddialogue.jsx
+++ src/components/founddialogue/Founddialogue.jsx
@@ -9,22 +9,8 @@ import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import Axios from "axios";
 import "./Founddialogue.css";
-import { styled } from '@mui/material/styles';
 
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
 export default function AlertDialog(props) {
-  const HtmlTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#f5f5f9',
-      color: 'rgba(0, 0, 0, 0.87)',
-      maxWidth: 220,
-      fontSize: theme.typography.pxToRem(12),
-      border: '1px solid #dadde9',
-    },
-  }));
   const [open, setOpen] = React.useState(false);
   const [open2, setOpen2] = React.useState(false);
 
@@ -44,17 +30,14 @@ export default function AlertDialog(props) {
   const [enteredNumber, setEnteredNumber] = useState("");
   const [enteredPlace, setEnteredPlace] = useState("");
   const [enteredAddress, setEnteredAddress] = useState("");
-  const [enteredObjId, setEnteredObjId] = useState('');
-
+  const [enteredObjId, setEnteredObjId] = useState("");
 
   const onNameChangehandler = (event) => setEnteredName(event.target.value);
   const onNumberChangehandler = (event) => setEnteredNumber(event.target.value);
   const onPlaceChangehandler = (event) => setEnteredPlace(event.target.value);
-  const onAddressChangehandler = (event) =>setEnteredAddress(event.target.value);
-  const onObjIdChangehandler = (event) =>setEnteredObjId(event.target.value);
-   
-   
-
+  const onAddressChangehandler = (event) =>
+    setEnteredAddress(event.target.value);
+  const onObjIdChangehandler = (event) => setEnteredObjId(event.target.value);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -79,16 +62,20 @@ export default function AlertDialog(props) {
     setEnteredNumber("");
     setEnteredObjId("");
     setOpen2(false);
-  
-   
   };
-  
+
   return (
     <>
       <div class="found">
         {" "}
-  
-        <button id="btnn" style={{background:props.color}} disabled={props.disabled} onClick={handleClickOpen}>{props.text}</button>
+        <button
+          id="btnn"
+          style={{ background: props.color }}
+          disabled={props.disabled}
+          onClick={handleClickOpen}
+        >
+          {props.text}
+        </button>
       </div>
 
       <Dialog
@@ -96,18 +83,29 @@ export default function AlertDialog(props) {
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        PaperProps={{
+          style: {
+            backgroundColor: "#706F6F",
+            color: "white",
+
+            borderRadius: 20,
+          },
+        }}
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Have you found this object"}
+        <DialogTitle id="alert-dialog-title" sx={{ fontFamily: "monospace" }}>
+          Have you found the object ?
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description"></DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleClickOpen2} autoFocus>
-            Found?
-          </Button>
+          <div class="found">
+            {" "}
+            <button onClick={handleClose}>Close</button>
+            <button onClick={handleClickOpen2} autoFocus>
+              Found?
+            </button>
+          </div>
         </DialogActions>
       </Dialog>
       {/* -------------- */}
@@ -116,31 +114,85 @@ export default function AlertDialog(props) {
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        PaperProps={{
+          style: {
+            backgroundColor: "#706F6F",
+            color: "white",
+          width:"400px",
+          
+            borderRadius: 20,
+          },
+        }}
       >
-        <DialogTitle id="alert-dialog-title">{"Details"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title" sx={{ fontFamily: "monospace" }}>
+          {"Details"}
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText  id="alert-dialog-description">
-          <TextField value={enteredName} onChange={onNameChangehandler}
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField margin="dense" value={enteredObjId} onChange={onObjIdChangehandler} fullWidth id="objid" label="Lost Object Id" type="text" varient="standard"/>
-        <TextField margin="dense" value={enteredNumber} onChange={onNumberChangehandler} fullWidth id="number" label="Mobile number" type="number" varient="standard"/>
-        
-        <TextField margin="dense" value={enteredPlace} onChange={onPlaceChangehandler} fullWidth id="placefound" label="Place of Found" type="text" varient="standard"/>
-        <TextField margin="dense" value={enteredAddress} onChange={onAddressChangehandler} fullWidth id="address" label="Address" type="text" varient="standard"/>
+          <DialogContentText id="alert-dialog-description">
+            <div className="new-expense__controls">
+              <div className="new-expense__control">
+                <label>Name</label>
+                <input
+                  id="name"
+                  label="Name"
+                  type="text"
+                  value={enteredName}
+                  onChange={onNameChangehandler}
+                />
+              </div>
+              <div className="new-expense__control">
+                <label>Object ID</label>
+                <input
+                  value={enteredObjId}
+                  onChange={onObjIdChangehandler}
+                  id="objid"
+                  label="Lost Object Id"
+                  type="text"
+                />
+              </div>
+              <div className="new-expense__control">
+                <label>Phone Number</label>
+                <input
+                  value={enteredNumber}
+                  onChange={onNumberChangehandler}
+                  id="number"
+                  label="Mobile number"
+                  type="number"
+                />
+              </div>
+              <div className="new-expense__control">
+                <label>Place</label>
+                <input
+                  value={enteredPlace}
+                  onChange={onPlaceChangehandler}
+                  id="placefound"
+                  label="Place of Found"
+                  type="text"
+                />
+              </div>
+
+              <div className="new-expense__control">
+                <label>Address</label>
+                <input
+                  id="address"
+                  label="Address"
+                  type="text"
+                  required
+                  value={enteredAddress}
+                  onChange={onAddressChangehandler}
+                />
+              </div>
+            </div>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={onFormSubmit} autoFocus>
-            Submit
-          </Button>
+          <div class="found">
+            {" "}
+            <button onClick={handleClose}>Close</button>
+            <button onClick={onFormSubmit} autoFocus>
+              Submit
+            </button>
+          </div>
         </DialogActions>
       </Dialog>
     </>
